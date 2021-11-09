09f7465f6fee0d04aa9da12e17477bbcf4763e4b
Julio <juliofreitas.bm@gmail.com>
2021-11-09 01:16:30 -0300

    Utilizando StyleSheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,67 @@
 import React, { Fragment } from 'react';
-import { View, Text } from 'react-native';
+import {
+	View,
+	SafeAreaView,
+	Text,
+	StyleSheet,
+	TextInput,
+	Platform,
+	TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
 	return (
-		<View style={{ 
-			flex: 1, 
-			justifyContent: 'center', 
-			alignItems: 'center'}}>
-			<Fragment>
-				<Text>Eu sou um texto dentro de um Fragment</Text>
-				<Text>Eu sou outro texto hehe</Text>
-			</Fragment>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.title}>Olá, Senhor Pimenta</Text>
+			<TextInput
+				style={styles.input}
+				placeholder="New skill"
+				placeholderTextColor="#555555"
+			/>
+			<TouchableOpacity
+				style={styles.button}
+				activeOpacity={.7}
+			>
+				<Text style={styles.buttonText}>Add</Text>
+			</TouchableOpacity>
+
+			<Text style={[styles.title, {marginTop: 50}]}>
+				My skills
+			</Text>
+		</SafeAreaView>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#121015',
+		paddingHorizontal: 30,
+		paddingVertical: 70,
+	},
+	title: {
+		color: '#FFFFFF',
+		fontSize: 24,
+		fontWeight: 'bold',
+	},
+	input: {
+		backgroundColor: '#1F1E25',
+		color: '#FFFFFF',
+		fontSize: 18,
+		padding: Platform.OS === 'IOS' ? 15 : 10,
+		marginTop: 30,
+		borderRadius: 7,
+	},
+	button: {
+		backgroundColor: '#A370F7',
+		padding: 15,
+		borderRadius: 7,
+		alignItems: 'center',
+		marginTop: 20,
+	},
+	buttonText: {
+		color: '#FFFFFF',
+		fontSize: 17,
+		fontWeight: 'bold'
+	}
+})
